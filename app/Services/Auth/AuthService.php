1020e6a2094696acb546a0f77bc9a7d0d3d584a1
MohamedHassan1996 <mohamedhassan00096@gmail.com>
2025-03-20 04:12:42 +0200

    t

--- app/Services/Auth/AuthService.php
+++ app/Services/Auth/AuthService.php
@@ -37,7 +37,7 @@ class AuthService
             }
 
             // // Revoke old tokens (optional)
-            $user->tokens()->delete();
+            //$user->tokens()->delete();
 
             // Generate a new token (DO NOT return it directly)
             $token = $user->createToken('auth_token')->plainTextToken;
