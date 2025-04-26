eee947d898c15e903d48cc2467a4cd3c17baddec
MohamedHassan1996 <mohamedhassan00096@gmail.com>
2025-04-27 02:02:46 +0300

    t

--- app/Models/CharityCaseChildren.php
+++ app/Models/CharityCaseChildren.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Models\CharityCase\CharityCase;
+use App\Models\Parameter\ParameterValue;
 use App\Traits\CreatedUpdatedBy;
 use Illuminate\Database\Eloquent\Model;
 
@@ -19,4 +21,19 @@ class CharityCaseChildren extends Model
         'education_level_id',
         'donation_type_id',
     ];
+
+    public function charityCase()
+    {
+        return $this->belongsTo(CharityCase::class);
+    }
+
+    public function educationLevel()
+    {
+        return $this->belongsTo(ParameterValue::class, 'education_level_id');
+    }
+
+    public function donationType()
+    {
+        return $this->belongsTo(ParameterValue::class, 'donation_type_id');
+    }
 }
